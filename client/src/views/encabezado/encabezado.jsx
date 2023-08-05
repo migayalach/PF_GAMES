cc64362e6787bed50780d1e81ba457dd51dead9d
herva99 <hernanvazquez722@gmail.com>
2023-08-04 22:57:31 -0300

    cards

--- client/src/views/encabezado/encabezado.jsx
+++ client/src/views/encabezado/encabezado.jsx
@@ -1,16 +1,25 @@
 import { NavLink } from "react-router-dom";
 import imagen from "./logogamer2.jpg"
+
+import styles from "./encabezado.module.css"
+
 export default function Encabezado() {
   return (
-    <div>
-      <div>
-        <img src={imagen} alt="logo" />
+    <div className={styles.enc}>
+      <div className={styles.logo}>
+        <img className={styles.image} src={imagen} alt="logo" />
         <h2>GAMING SHOP</h2>
       </div>
-      <nav>
-        <NavLink to="/soporte">SOPORTE</NavLink>
-        <NavLink to="/compras">COMPRAS</NavLink>
-        <NavLink to="/perfil">PERFIL</NavLink>
+      <nav >
+        <a className={styles.encab}>
+        <NavLink to="/soporte" style={{ textDecoration: "none", color: "black" }} >SOPORTE</NavLink>
+        </a>
+        <a className={styles.encab}>
+        <NavLink to="/compras" style={{ textDecoration: "none", color: "black" }}>COMPRAS</NavLink>
+        </a>
+        <a className={styles.encab}>
+        <NavLink to="/perfil" style={{ textDecoration: "none", color: "black" }}>PERFIL</NavLink>
+        </a>
       </nav>
     </div>
   );
