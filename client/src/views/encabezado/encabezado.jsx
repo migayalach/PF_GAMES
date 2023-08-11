88720b191fe08a542073f2f1708135d86ad24341
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-08-10 22:30:17 -0400

    Un pequeño ejemplo de como esta el resultado segun el nivel de acceso

--- client/src/views/encabezado/encabezado.jsx
+++ client/src/views/encabezado/encabezado.jsx
@@ -1,11 +1,28 @@
 import { NavLink } from "react-router-dom";
-import imagen from "./logogamer2.jpg"
+import imagen from "./logogamer2.jpg";
 
-import styles from "./encabezado.module.css"
+import styles from "./encabezado.module.css";
+
+//***********************************
+import { useAuth0 } from "@auth0/auth0-react";
+import Login from "../../components/Login/Login";
+import Logout from "../../components/Logout/Logout";
+import Profile from "../../components/Profile/Profile";
+import { useSelector } from "react-redux";
+//***********************************
 
 export default function Encabezado() {
+  const aux = useSelector((state) => state.levelUser);
+  const acceso = aux.access;
+
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) return <h1>Cargando ................</h1>;
   return (
     <div className={styles.enc}>
+      <Profile />
+      {acceso === "admin" && <button>hola</button>}
+      {acceso === "standar" && <button>chau</button>}
+
       <div className={styles.logo}>
         <img className={styles.image} src={imagen} alt="logo" />
         <h2>GAMING SHOP</h2>
@@ -22,6 +39,7 @@ export default function Encabezado() {
         <NavLink to="/perfil" style={{ textDecoration: "none", color: "black" }}>PERFIL</NavLink>
         </a>
       </nav>
+      {isAuthenticated ? <Logout /> : <Login />}
     </div>
   );
-};
+}
