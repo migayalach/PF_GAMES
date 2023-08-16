7fd8c971682bb367bf268f82b954fe091354dfe1
Miguel E. Ayala Chávez <89620485+migayalach@users.noreply.github.com>
2023-08-15 22:14:43 -0400

    Update index.js
    force

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const { sequelize } = require("./src/DataBase/dataBase");
 require('dotenv').config();
 
 app.listen(process.env.PORT, () => {
-  sequelize.sync({ alter: true });
+  sequelize.sync({ force: true });
   console.log(`Servidor levantado en el puerto: ${process.env.PORT}`);
 });
