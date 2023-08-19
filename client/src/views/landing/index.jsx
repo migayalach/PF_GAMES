6e819cbc937943d40544c24b86f104e39100660b
GavidiaS <gavdia1302@gmail.com>
2023-08-19 17:35:11 -0500

    Dashboard Funcional solo falta probar ventas

--- client/src/views/landing/index.jsx
+++ client/src/views/landing/index.jsx
@@ -1,16 +1,24 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { gamesByGenders } from "../../redux/actions";
 import Encabezado from "../encabezado/encabezado";
 import NavBar from "../../components/NavBar/NavBar";
 import Carousel from '../../components/Carousel/Carousel';
 
 export default function Landing() {
-  const { action, sports, adventure } = useSelector(state => state.gamesByGenres);
+  const dispatch = useDispatch();
+  const { rpg, sports, shooter } = useSelector(state => state.gamesByGenres);
+  useEffect(() => {
+    dispatch(gamesByGenders("RPG"));
+    dispatch(gamesByGenders("Sports"));
+    dispatch(gamesByGenders("Shooter"));
+  }, []);
   return (
     <div>
       <Encabezado/>
       <NavBar/>
-      <Carousel images={action}/>
-      <Carousel images={adventure}/>
+      <Carousel images={rpg}/>
+      <Carousel images={shooter}/>
       <Carousel images={sports}/>
     </div>
   );
